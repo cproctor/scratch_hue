058b90158ad0a79e7e62f8014d5effc2c1aef6ea
Chris Proctor <chris.proctor@gmail.com>
2015-02-02 23:36:32 -0800

    Getting ready to push to pypi

---
+++ README.md
@@ -0,0 +1,8 @@
+Scratch Hue Extension
+---------------------
+
+This package provides a helper app to allow the Scratch 2 Offline Editor to 
+control a set of Philips Hue lights. Currently, only three lights are supported,
+but more could be added  by configuration if necessary.
+
+Instructions for running a helper app are at http://wiki.scratch.mit.edu/w/images/ExtensionsDoc.HTTP-9-11.pdf.
--- setup.py
+++ setup.py
@@ -2,13 +2,18 @@ from setuptools import setup
  
 setup(
     name = 'scratch_hue_extension',
-    packages = ['scratch_hue_extension'],
+    # packages = ['scratch_hue_extension'],
+    py_modules = ['scratch_hue_extension'],
+    scripts = ['scratch_hue_extension'],
+    install_requires = ['phue', 'flask'],
+    package_data = {
+        '': ['COPYING.txt', 'LICENSE', 'README.md', 'scratch_hue_extension.s2e', 'setup.cfg']
+    },
     version = '0.0.1',
     description = 'Provides a http helper app to allow Scratch 2 to control Philips Hue lights',
     author='Chris Proctor',
     author_email='chris.proctor@gmail.com',
     url='http://mrproctor.net/scratch',
-    download_url='https://github.com/cproctor/scratch_hue/tarball/0.1',
     classifiers=[
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
